8d3239cd0bfda660fd537442fc132949238cba1e
wellbeing-dough <kdw990202@naver.com>
2024-07-06 14:53:28 +0900

    설명 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,17 @@ const apiUrl = 'https://file-extension-blocker-wellbeing-dough.store/api';
 const customExtensionMaxCount = 200;
 
 function App() {
+  // customTag 차단된 커스텀 태그 리스트들 배열
   const [customTags, setCustomTags] = useState([]);
+  // fixedTags 이게 전체 고정 차단 태그 리스트 배역
   const [fixedTags, setFixedTags] = useState([]);
+  // fixedTagsAll 차단된 고정 차단 태그 리스트 배열
   const [fixedTagsAll, setFixedTagsAll] = useState([]);
+  // input값 string
   const [inputValue, setInputValue] = useState('');
+  // 전체 체크박스 상태 객체
   const [selectedExtensions, setSelectedExtensions] = useState({});
+  // 화면에 뿌려지는 에러 메시지 상태 string
   const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
@@ -21,7 +27,7 @@ function App() {
     const customExtensionUrl = `${apiUrl}/v1/file/all/block-extensions`;
     const fixedExtensionUrl = `${apiUrl}/v1/file/fixed/block-extensions`;
 
-    Promise.all([
+    Promise.all([ //api 두번 첫번째 반환값을 두번째 호출값에 써야할 경우 사용
       axios.get(customExtensionUrl),
       axios.get(fixedExtensionUrl)
     ])
@@ -41,20 +47,19 @@ function App() {
   };
 
   const updateSelectedExtensions = (fixedExtensionsAll, fixedExtensions) => {
-    const fixedExtensionsMap = fixedExtensionsAll.reduce((acc, ext) => {
-      acc[ext.extensionName.toLowerCase()] = false;
+    const fixedExtensionsMap = fixedExtensionsAll.reduce((acc, ext) => { //reduce 는 배열에서 한번씩 돌면서 안에 함수를 실행
+      acc[ext.extensionName.toLowerCase()] = false;   // 소문자로 바꿔주고 기본값 체크박스 해제로 설정
       return acc;
     }, {});
 
     const fixedExtNames = fixedExtensions.map(ext => ext.extensionName.toLowerCase());
-    fixedExtNames.forEach(ext => {
+    fixedExtNames.forEach(ext => {     //fixedExtNames돌면서 함수를 실행할 뿐이지 return 하진않음
       if (fixedExtensionsMap.hasOwnProperty(ext)) {
         fixedExtensionsMap[ext] = true;
       }
     });
 
     setSelectedExtensions(fixedExtensionsMap);
-    localStorage.setItem('selectedExtensions', JSON.stringify(fixedExtensionsMap));
   };
 
   const handleAddTag = (extensionName) => {
@@ -103,16 +108,16 @@ function App() {
 
   const handleCheckboxChange = (extension) => {
     const newSelectedExtensions = {
-      ...selectedExtensions,
-      [extension]: !selectedExtensions[extension],
+      ...selectedExtensions,  //원래 있떤 체크박스는 유지
+      [extension]: !selectedExtensions[extension],  // !표는 반대라는 뜻인데 체크되어있으면 체크 풀고 체크 안되어있으면 체크 하라
     };
-    setSelectedExtensions(newSelectedExtensions);
-    localStorage.setItem('selectedExtensions', JSON.stringify(newSelectedExtensions));
+    setSelectedExtensions(newSelectedExtensions);   // setSelectedExtensions메서드에 붙혀넣기 state를 변경되야 화면이 바뀌니까
 
-    if (newSelectedExtensions[extension]) {
+
+    if (newSelectedExtensions[extension]) {   //만약에 체크를 새로 했으면 handleAddTag
       handleAddTag(extension);
-    } else {
-      const tagToRemove = fixedTagsAll.find(tag => tag.extensionName.toLowerCase() === extension);
+    } else {  // 안했으면 제거해라
+      const tagToRemove = fixedTagsAll.find(tag => tag.extensionName.toLowerCase() === extension);  // fixedTagsAll에 있는 상태 중에서 이름이랑 똑같은 애가 있는지(뭘 제거해야되는지 판단 기ㅏ준은 extensionName)
       if (tagToRemove) {
         handleRemoveTag(tagToRemove);
       }
